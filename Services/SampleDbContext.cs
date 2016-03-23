eafd3e8e1ae6260f371bf6017116e934b062340e
jsakamoto <junichi.jp@gmail.com>
2016-03-24 00:32:50 +0900

    Create DbContext class and configure connection strings for access MySql database.

---
+++ Services/SampleDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using UsingJSONTextFileAsADataStore.Models;
+
+namespace UsingJSONTextFileAsADataStore.Services
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class SampleDbContext : System.Data.Entity.DbContext
+    {
+        public System.Data.Entity.DbSet<Person> People { get; set; }
+    }
+}
